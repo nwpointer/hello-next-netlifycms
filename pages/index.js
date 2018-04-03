787f8fa9cd46865bd53e6ae0d23e2dc748f0eefd
admin <admin@nwpointer.local>
2018-04-02 20:47:55 -0700

    testimonials

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,17 @@ import Nav from '../components/Nav'
 import ContentLink from '../components/ContentLink'
 import { listFromRecords} from '../mapContent'
 import posts from '../_content/posts.json'
+import testimonials from '../_content/testimonials.json'
 
 const PostLink = (props, i) => (
   <li key={i} >
     <ContentLink type="post" record={props} />
   </li>
 )
+
+const Testimonial = ({body}, i) => {
+  return <div>{body}</div>
+}
 
 export default class extends Component {
   render() {
@@ -18,6 +23,8 @@ export default class extends Component {
         <Nav />
         <p>Hello Next.js!!</p>
         { listFromRecords(posts).map(PostLink)}
+        { console.log(testimonials) } 
+        { listFromRecords(testimonials).map(Testimonial)}
       </div>
     )
   }
